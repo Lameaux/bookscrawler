cf42d97517540ceb82c9bb0e9397caf4b1a72bd9
Sergii Sizov <sergii.sizov@gmail.com>
2015-09-06 14:08:37 +0200

    reverse index

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS book (
 ) ENGINE=InnoDB;
 CREATE INDEX book_author_id on book(author_id);
 CREATE INDEX book_genre on book(genre);
-CREATE INDEX book_rating on book(rating);
+CREATE INDEX book_rating_desc on book(rating DESC);
 
 CREATE TABLE IF NOT EXISTS comment (
 	id INT auto_increment PRIMARY KEY,
